d9c0b727e11064a3068103163b18c48d1cb9ca00
pxcvbe <hackedby1234567@gmail.com>
2025-12-02 15:20:27 +0700

    Update 'app/__init__.py' Initiate Flask App

---
+++ api-server/app/__init__.py
@@ -0,0 +1,17 @@
+# ====================
+# FILE: app/__init__.py
+# ====================
+from flask import Flask
+from flask_cors import CORS
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Enable CORS untuk semua routes
+    CORS(app, resources={r"/v1/api/*": {"origins": "*"}})
+    
+    # Register blueprints
+    from app.routes import api
+    app.register_blueprint(api, url_prefix='v1/api')
+    
+    return app
